3d3c7be72e78ee57c1fcf2a260131e0ea3b2e7e4
Joeschmoe7 <loukrawczyk1@gmail.com>
2022-02-21 20:25:27 -0500

    SQL insert still not working

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
-import pandas as pd
 import sqlite3
 
-r = []     #Initialize list for files in breach
-database = r"c:\test\account_database"     #Database location
+fileList = []     #Initialize list for files in breach
+database = r"c:\test\account_database.db"     #Database location
 breachLocation = r"C:\Users\Lou\Downloads\CompilationOfManyBreaches\data\0\""    #Location of breach file
 
 def createDatabase():  #Creates database with Username and Password tables
@@ -12,7 +11,7 @@ def createDatabase():  #Creates database with Username and Password tables
     c = conn.cursor()
     c.execute('''
               CREATE TABLE IF NOT EXISTS Accounts
-              (Username TEXT, Password TEXT)
+              (Username text, Password text)
               ''')
     conn.commit()
 
@@ -22,19 +21,25 @@ def list_files(dir):      #Creates a list of files in the breach folder
             r.append(os.path.join(root, name))
     return r
 
+dirName = '/Users/krunal/desktop/code/pyt'
+listOfFiles = getFiles(dirName)
+print(listOfFiles)
+
 username = []     #Initialize list for usernames
 password = []     #Initialize list for passwords
 
 def createLists():     #Populate lists from text files
+    #conn = sqlite3.connect(database)
+    #c = conn.cursor()
+    print(r)
     for files in r:
-        with open (files, encoding = "UTF-8") as x:
-             for lines in x:
-                 left, right= lines.rsplit(":", 1)
-                 username.append(left)
-                 password.append(right)
-
-        username.clear()
-        password.clear()
+        with open(files, encoding = "UTF-8") as x:
+            for lines in x:
+                left, right = lines.rsplit(":", 1)
+                #c.execute("INSERT INTO Accounts (Username, Password) VALUES(?,?)", (left, right))
+                #conn.commit()
+                print(left)
+    #conn.close()
 
 createDatabase()
 list_files(breachLocation)
