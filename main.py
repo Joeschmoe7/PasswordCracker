39b4d2ad2418742a6b1e40417adaad4802b6e6ad
Joeschmoe7 <loukrawczyk1@gmail.com>
2022-02-22 19:12:22 -0500

    SQLite DB updating!

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import sqlite3
 
-fileList = []     #Initialize list for files in breach
-database = r"c:\test\account_database.db"     #Database location
-breachLocation = r"C:\Users\Lou\Downloads\CompilationOfManyBreaches\data\0\""    #Location of breach file
+fileList = []     # Initialize list for files in breach
+database = r"c:\test\account_database.db"     # Database location
+breachLocation = r"C:\Users\Lou\Downloads\CompilationOfManyBreaches\data\1\\"    # Location of breach file
+r = []
 
-def createDatabase():  #Creates database with Username and Password tables
+
+def create_database():  # Creates database with Username and Password tables
     import sqlite3
     conn = sqlite3.connect(database)
     c = conn.cursor()
@@ -15,32 +17,32 @@ def createDatabase():  #Creates database with Username and Password tables
               ''')
     conn.commit()
 
-def list_files(dir):      #Creates a list of files in the breach folder
+
+def list_files(dir):      # Creates a list of files in the breach folder
     for root, dirs, files in os.walk(dir):
         for name in files:
             r.append(os.path.join(root, name))
     return r
 
-dirName = '/Users/krunal/desktop/code/pyt'
-listOfFiles = getFiles(dirName)
-print(listOfFiles)
+list_files(breachLocation)
 
-username = []     #Initialize list for usernames
-password = []     #Initialize list for passwords
+username = []     # Initialize list for usernames
+password = []     # Initialize list for passwords
 
-def createLists():     #Populate lists from text files
-    #conn = sqlite3.connect(database)
-    #c = conn.cursor()
+
+def create_lists():     # Populate lists from text files
+    conn = sqlite3.connect(database)
+    c = conn.cursor()
     print(r)
     for files in r:
         with open(files, encoding = "UTF-8") as x:
             for lines in x:
                 left, right = lines.rsplit(":", 1)
-                #c.execute("INSERT INTO Accounts (Username, Password) VALUES(?,?)", (left, right))
-                #conn.commit()
-                print(left)
-    #conn.close()
+                c.execute("INSERT INTO Accounts (Username, Password) VALUES(?,?)", (left, right))
+                conn.commit()
 
-createDatabase()
+    conn.close()
+
+create_database()
 list_files(breachLocation)
-createLists()
+create_lists()
